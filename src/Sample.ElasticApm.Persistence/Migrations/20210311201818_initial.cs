cd848e239eecac8aab1b2dea510b7b92d6ebe7ef
Henrique Mauri <hgmauri@gmail.com>
2021-11-29 06:32:42 -0300

    fix

--- src/Sample.ElasticApm.Persistence/Migrations/20210311201818_initial.cs
+++ src/Sample.ElasticApm.Persistence/Migrations/20210311201818_initial.cs
@@ -1,31 +1,30 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace Sample.ElasticApm.Persistence.Migrations
-{
-    public partial class initial : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Pessoas",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Pessoas", x => x.Id);
-                });
-        }
+namespace Sample.ElasticApm.Persistence.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Pessoas");
-        }
+public partial class initial : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Pessoas",
+            columns: table => new
+            {
+                Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                Endereco = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Pessoas", x => x.Id);
+            });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Pessoas");
     }
 }
